f99f9ca699d04df7f2db89fdcfb6c054026f4968
beardoll <562969421@qq.com>
2016-10-07 20:44:47 +0800

    重新安装了一下NPM，可以运行。

--- src/components/Gate.vue
+++ src/components/Gate.vue
@@ -11,9 +11,9 @@
     }
 </style>
 <script>
-  var qs = require('querystring')
+/*  var qs = require('querystring')*/
   export default {
-    created: function () {
+/*    created: function () {
       let params = qs.parse(window.location.href.split('?')[1])
       if (params.code) {
         this.$http.get('http://mecord.cn:3000/api/MecordUsers/wxLogin?code=' + params.code)
@@ -26,12 +26,12 @@
           })
           // this.$router.go('userinterface/:0')
       }
-    }
-/*    ready: function () {
+    }*/
+    ready: function () {
       console.log('begin login!')
       this.$root.login('LVsy17Tx0nKNEewIXbA46g4MFXVlvUKLb7j4qA4jfp13bYzR1HSCktcYmQ3yn8Jo')
       this.$root.loadClientDate()
-    }*/
+    }
   }
 </script>
 x
